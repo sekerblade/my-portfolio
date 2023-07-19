562e385dd1241e9b09c263ab654d04b6529eee96
safe_pongsathorn <pongsathorn.sut@dome.tu.ac.th>
2023-07-19 20:08:36 +0700

    fix sticky small device

--- src/components/sections/leftSection/index.jsx
+++ src/components/sections/leftSection/index.jsx
@@ -5,7 +5,7 @@ import Navbar from "../navbar";
 const LeftSection = () => {
   return (
     <>
-      <div className="sticky top-14 grid gap-y-5 pl-10 pr-5 lg:grid-rows-[35%_45%_20%] lg:h-[87vh]">
+      <div className="lg:sticky top-14 grid gap-y-5 pl-10 pr-5 lg:grid-rows-[35%_45%_20%] lg:h-[87vh]">
         <Header />
         <Navbar />
         <Contact />
